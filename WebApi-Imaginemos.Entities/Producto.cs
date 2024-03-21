b9d6bfbbce014b7f721ed1fda6a91ad1aa2b2713
Deyson Venté <deiandresvent@hotmail.com>
2024-03-20 21:11:04 -0500

    se modificó la entidad: Campo Presion cambiado a Presio

--- WebApi-Imaginemos.Entities/Producto.cs
+++ WebApi-Imaginemos.Entities/Producto.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
-        public decimal Precion { get; set; }
+        public decimal Precio { get; set; }
         public string Descripcion { get; set; }
     }
 }
