4f5b21fba288d5635740a585bfffbe2198eb92cb
Deyson Venté <deiandresvent@hotmail.com>
2024-03-21 20:21:47 -0500

    se ha cambiado el venta detalle a un arreglo de enteros, aun sin implementar.

--- WebApi-Imaginemos-DTOs/VentaDetalleUsuario.cs
+++ WebApi-Imaginemos-DTOs/VentaDetalleUsuario.cs
@@ -6,7 +6,7 @@ namespace WebApi_Imaginemos_DTOs
     {
         public int Id { get; set; }
         public int UsuarioId { get; set; }
-        public int VentaDetalleId { get; set; }
+        public int[] VentaDetalleId { get; set; }
         public string NombreUsuario { get; set; }
         public DateTime Fecha { get; set; }
         public decimal Total { get; set; }
